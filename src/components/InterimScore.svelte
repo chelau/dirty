fc8d2cf23fd31b094d3fb4451cd3271ad4d30b94
chelau <96844282+chelau@users.noreply.github.com>
2022-01-22 05:24:14 +0100

    Update InterimScore.svelte

--- src/components/InterimScore.svelte
+++ src/components/InterimScore.svelte
@@ -76,7 +76,7 @@
                 <div class="col-12 col-md-8 col-lg-6 mb-3">
                     <div class="card bg-blue" on:click="{() => startTime()}">
                         <div class="card-body text-center">
-                            <h2 class="c-white mb-0">Start de tijd</h2>
+                            <h2 class="c-red mb-0">Start de tijd</h2>
                         </div>
                     </div>
                 </div>
